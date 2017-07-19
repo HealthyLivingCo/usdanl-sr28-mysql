39373fe04c342e50156abb74256950f6320c9027
mirocow <Mirocow@users.noreply.github.com>
2017-07-19 20:05:14 +0300

    Update sr28_import.sh

--- sr28_import.sh
+++ sr28_import.sh
@@ -11,38 +11,39 @@ dos2unix *.txt
 MYSQL_HST=localhost
 MYSQL_USR=$1
 MYSQL_PWD=$2
+MYSQL_DATABASE=$3
 
 # create database with predefined schema
 mysql -u${MYSQL_USR} -p${MYSQL_PWD} < ./sr28_schema.sql &&
 
-# the following data imports only work, if mysql permits to import 
-# these files. To prepare for the correct permission, path to CSV 
-# files must be added to apparmor config, and apparmor config must 
+# the following data imports only work, if mysql permits to import
+# these files. To prepare for the correct permission, path to CSV
+# files must be added to apparmor config, and apparmor config must
 # be reloaded.
 #
-#   1) sudo vim /etc/apparmor.d/usr.sbin.mysqld 
+#   1) sudo vim /etc/apparmor.d/usr.sbin.mysqld
 #   2) add line "/path/to/csv/ rw" to file within curly brackets
 #   3) sudo /etc/init.d/apparmor reload
 #
 
 HOME=`pwd`;
 # import USDA SR-28 data files into predefined table schema
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/FD_GROUP.txt' INTO TABLE FD_GROUP FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/FOOD_DES.txt' INTO TABLE FOOD_DES FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/DATA_SRC.txt' INTO TABLE DATA_SRC FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/DERIV_CD.txt' INTO TABLE DERIV_CD FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/FOOTNOTE.txt' INTO TABLE FOOTNOTE FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/LANGDESC.txt' INTO TABLE LANGDESC FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/WEIGHT.txt' INTO TABLE WEIGHT FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/NUTR_DEF.txt' INTO TABLE NUTR_DEF FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/LANGUAL.txt' INTO TABLE LANGUAL FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/SRC_CD.txt' INTO TABLE SRC_CD FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/DATSRCLN.txt' INTO TABLE DATSRCLN FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
-mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28; LOAD DATA INFILE '${HOME}/NUT_DATA.txt' INTO TABLE NUT_DATA FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/FD_GROUP.txt' INTO TABLE FD_GROUP FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/FOOD_DES.txt' INTO TABLE FOOD_DES FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/DATA_SRC.txt' INTO TABLE DATA_SRC FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/DERIV_CD.txt' INTO TABLE DERIV_CD FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/FOOTNOTE.txt' INTO TABLE FOOTNOTE FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/LANGDESC.txt' INTO TABLE LANGDESC FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/WEIGHT.txt' INTO TABLE WEIGHT FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/NUTR_DEF.txt' INTO TABLE NUTR_DEF FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/LANGUAL.txt' INTO TABLE LANGUAL FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/SRC_CD.txt' INTO TABLE SRC_CD FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/DATSRCLN.txt' INTO TABLE DATSRCLN FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
+mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE}; LOAD DATA INFILE '${HOME}/NUT_DATA.txt' INTO TABLE NUT_DATA FIELDS TERMINATED BY '^' ENCLOSED BY '\~';" &&
 
 echo "USDA National Nutrient Database imported successfully"
-rm *.txt *.zip 
+rm *.txt *.zip
 echo "Clean up complete."
 
 # run test query
-# mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE usdanlsr28;select * from NUT_DATA;"
+# mysql -u${MYSQL_USR} -p${MYSQL_PWD} -e "USE ${MYSQL_DATABASE};select * from NUT_DATA;"
